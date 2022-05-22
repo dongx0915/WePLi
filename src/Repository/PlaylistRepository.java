bd771ce12efc3494882435b3068a67518fb66643
pray <sjh990519@gmail.com>
2022-05-23 00:25:34 +0900

    SongChart 이름 수정

--- src/Repository/PlaylistRepository.java
+++ src/Repository/PlaylistRepository.java
@@ -12,4 +12,9 @@ import Entity.Playlist;
  */
 public class PlaylistRepository extends EntityRepository<Playlist, String>{
     public PlaylistRepository() { super.setEntity(new Playlist());}
+    
+
+    // 플레이리스트 담은 곡 삭제문 생성
+    
+    
 }
